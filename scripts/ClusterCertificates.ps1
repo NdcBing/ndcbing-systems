b40cf0d480c269a145606a97356fd06601931ebf
Louis DeJardin <lodejard@microsoft.com>
2017-07-30 15:12:39 -0700

    Troubleshooting certificate creation

--- scripts/ClusterCertificates.ps1
+++ scripts/ClusterCertificates.ps1
@@ -1,23 +1,20 @@
 [CmdletBinding()]
 Param(
   [Parameter(Mandatory=$False)]
-  [string]$location = "eastus2"
+  [string] $location = "eastus2"
 )
 
-$resourceGroupLocation = "eastus2"
-$resourceGroupName = "fabric-keyvaults"
+$resourceGroupName = "ndcbing-$location-kv"
 $keyVaultName = "ndcbing-$location-kv"
+
 $dnsName = "$location.ndcbing.com"
-$localCertPath = "."
-$newCertName = "$dnsName"
+$localCertPath = "$PSScriptRoot"
+$newCertName = "$location-ndcbing-com"
+$password = "Secret5!"
 
 $azureRmContext = Get-AzureRmContext
 $subscriptionId = $azureRmContext.Subscription.SubscriptionId
 
 Import-Module "$PSScriptRoot\..\vendor\chackdan\Scripts\ServiceFabricRPHelpers\ServiceFabricRPHelpers.psm1"
 
-New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation -Force
-
-New-AzureRmKeyVault -ResourceGroupName $resourceGroupName -VaultName $keyVaultName -Location $location -EnabledForDeployment -Force
-
-Invoke-AddCertToKeyVault -SubscriptionId $subscriptionId -ResourceGroupName $resourceGroupName -Location $location -VaultName $keyVaultName -CertificateName $newCertName -CreateSelfSignedCertificate -DnsName $dnsName -OutputPath $localCertPath
+Invoke-AddCertToKeyVault -SubscriptionId $subscriptionId -ResourceGroupName $resourceGroupName -Location $location -VaultName $keyVaultName -CertificateName $newCertName -CreateSelfSignedCertificate -DnsName $dnsName -OutputPath $localCertPath -Password $password
