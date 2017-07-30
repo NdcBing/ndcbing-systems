0a1c0dea8c856fd3af79f4c479f5cc124889df69
Louis DeJardin <lodejard@microsoft.com>
2017-07-30 15:41:20 -0700

    Setting access policy for VSTS apply changes

--- scripts/ClusterCertificates.ps1
+++ scripts/ClusterCertificates.ps1
@@ -1,20 +1,42 @@
 [CmdletBinding()]
 Param(
   [Parameter(Mandatory=$False)]
-  [string] $location = "eastus2"
+  [string] $Location = "eastus2"
 )
 
-$resourceGroupName = "ndcbing-$location-kv"
-$keyVaultName = "ndcbing-$location-kv"
+$ResourceGroupName = "ndcbing-$location-kv"
+$KeyVaultName = "ndcbing-$location-kv"
 
-$dnsName = "$location.ndcbing.com"
-$localCertPath = "$PSScriptRoot"
-$newCertName = "$location-ndcbing-com"
-$password = "Secret5!"
+$DnsName = "$location.ndcbing.com"
+$LocalCertPath = "$PSScriptRoot"
+$NewCertName = "$location-ndcbing-com"
+$Password = "Secret5!"
 
-$azureRmContext = Get-AzureRmContext
-$subscriptionId = $azureRmContext.Subscription.SubscriptionId
+$AzureRmContext = Get-AzureRmContext
+$SubscriptionId = $azureRmContext.Subscription.SubscriptionId
+
+Write-Host "Ensuring ResourceGroup $ResourceGroupName in $Location"
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location -Force | Out-Null
+
+try
+{
+    $existingKeyVault = Get-AzureRmKeyVault -VaultName $KeyVaultName -ResourceGroupName $ResourceGroupName
+    $resourceId = $existingKeyVault.ResourceId
+    Write-Host "Using existing vault $VaultName in $($existingKeyVault.Location)"
+}
+catch
+{
+}
+
+if(!$existingKeyVault)
+{
+    Write-Host "Creating new vault $VaultName in $location"
+    $newKeyVault = New-AzureRmKeyVault -VaultName $VaultName -ResourceGroupName $ResourceGroupName -Location $Location -EnabledForDeployment
+    $resourceId = $newKeyVault.ResourceId
+}
+
+Set-AzureKeyVaultAccessPolicy -VaultName $VaultName -ObjectId "d64ab969-c499-48fe-8919-1b537990d064" -PermissionsToKeys all -PermissionsToSecrets all -PermissionsToCertificates all
 
 Import-Module "$PSScriptRoot\..\vendor\chackdan\Scripts\ServiceFabricRPHelpers\ServiceFabricRPHelpers.psm1"
 
-Invoke-AddCertToKeyVault -SubscriptionId $subscriptionId -ResourceGroupName $resourceGroupName -Location $location -VaultName $keyVaultName -CertificateName $newCertName -CreateSelfSignedCertificate -DnsName $dnsName -OutputPath $localCertPath -Password $password
+Invoke-AddCertToKeyVault -SubscriptionId $SubscriptionId -ResourceGroupName $ResourceGroupName -Location $Location -VaultName $KeyVaultName -CertificateName $NewCertName -CreateSelfSignedCertificate -DnsName $DnsName -OutputPath $LocalCertPath -Password $Password
