cc20344ebf74e5bd5ed8d0943ff08fed1fde71c5
Louis DeJardin <lodejard@microsoft.com>
2017-08-02 11:46:41 -0700

    Adding AAD SF App creation automation

---
+++ scripts/CreateApp.ps1
@@ -0,0 +1,18 @@
+$tenantId = (Get-AzureRmContext).Subscription.TenantId
+$locations = @("westus2", "eastus2")
+$appName = "NDCBingProductionCluster"
+
+$ConfigObj = .\MicrosoftAzureServiceFabric-AADHelpers\SetupApplications.ps1 `
+    -TenantId $tenantId `
+    -WebApplicationReplyUrl ( $locations | %{"https://$_.ndcbing.com/"} ) `
+    -WebApplicationName $appName `
+    -NativeClientApplicationName "$appName-NativeClient" `
+    -WebApplicationUri "https://$appName"
+
+$locations | %{
+    $KeyVaultName = "ndcbing-$_-kv"
+
+    Set-AzureKeyVaultSecret -VaultName $KeyVaultName -Name "$appName-TenantId" -SecretValue (ConvertTo-SecureString -String $ConfigObj.TenantId -AsPlainText -Force)
+    Set-AzureKeyVaultSecret -VaultName $KeyVaultName -Name "$appName-WebAppId" -SecretValue (ConvertTo-SecureString -String $ConfigObj.WebAppId -AsPlainText -Force)
+    Set-AzureKeyVaultSecret -VaultName $KeyVaultName -Name "$appName-NativeClientAppId" -SecretValue (ConvertTo-SecureString -String $ConfigObj.NativeClientAppId -AsPlainText -Force)
+}
